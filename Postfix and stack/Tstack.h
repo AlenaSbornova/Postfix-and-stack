44c8346f42cec4342ae822cecb0a7cba213bfbe3
sfmggsmgigomjn <154761994+sfmggsmgigomjn@users.noreply.github.com>
2023-12-28 19:36:08 +0300

    q

--- Postfix and stack/Tstack.h
+++ Postfix and stack/Tstack.h
@@ -1,5 +1,52 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+
 class Tstack
 {
+private:
+	int r = 50;
+	int k; //кол-во эл-тов лежащих в стеке
+	int a[50];
+public:
+	Tstack() {
+		k = -1;
+		a[0] = 0;
+	}
+	char view() {
+		if (k <= (-1))
+			cout << "Ошибка, попытка просмора из пустого стека";
+		else
+			return a[k];
+
+	}
+	Tstack put(char c) {
+		if (r <= k++) {
+			cout << "Ошибка, переполнение стека";
+		}
+		a[k] = c;
+		return *this;
+	}
+	char get() {
+		k = k - 1;
+		return a[k + 1];
+	}
+	Tstack operator=(Tstack st) {
+		k = st.k;
+		for (int i = 0; i <= k; i++) {
+			a[i] = st.a[i];
+		}
+		return *this;
+	}
+	Tstack copy(Tstack st) {
+		*this = st;
+	}
+	int isEmpty() {
+		if (k == -1)
+			return 1;
+		else
+			return 0;
+	}
 };
 
--- Postfix and stack/test.cpp
+++ Postfix and stack/test.cpp
@@ -1,6 +1,65 @@
 #include "pch.h"
+#include "Tpostfix.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TectingCorrectValue, 2plas3) {
+	Tpostfix p("2+3");
+	int r;
+	r = p.calculate();
+	EXPECT_EQ(5, r);
+}
+TEST(TectingCorrectValue, 2plas3mines4) {
+	Tpostfix p("2+3-4");
+	int r, rr;
+	r = p.calculate();
+	rr = 2 + 3 - 4;
+	EXPECT_EQ(rr, r);
+}
+TEST(TectingCorrectValue, TestBrackets) {
+	Tpostfix p("5-(2+2)");
+	int r, rr;
+	r = p.calculate();
+	rr = 5 - (2 + 2);
+	EXPECT_EQ(rr, r);
+}
+TEST(TectingCorrectValue, TestLongExpression) {
+	Tpostfix p("5-(2+2)-3*5+4/2-1+6-8*1");
+	int r, rr;
+	r = p.calculate();
+	rr = 5 - (2 + 2) - 3 * 5 + 4 / 2 - 1 + 6 - 8 * 1;
+	EXPECT_EQ(rr, r);
+}
+TEST(TectingCorrectValue, TestManyBrackets) {
+	Tpostfix p("5-(((2+2)*3-8/(3+1))-(2-5/(2-1)))");
+	int r, rr;
+	r = p.calculate();
+	rr = 5 - (((2 + 2) * 3 - 8 / (3 + 1)) - (2 - 5 / (2 - 1)));
+	EXPECT_EQ(rr, r);
+}
+TEST(TectingErrors, UncorrectSymbol) {
+	Tpostfix p("2+r");
+	int e;
+	p.calculate();
+	e = p.status();
+	EXPECT_EQ(8, e);
+}
+TEST(TectingErrors, UncorrectBrackets) {
+	Tpostfix p("2-(3-2(");
+	int e;
+	p.calculate();
+	e = p.status();
+	EXPECT_EQ(6, e);
+}
+TEST(TectingErrors, UncorrectLexOrder) {
+	Tpostfix p("2-(3-+2)");
+	int e;
+	p.calculate();
+	e = p.status();
+	EXPECT_EQ(7, e);
+}
+TEST(TectingErrors, EmptyString) {
+	Tpostfix p("");
+	int e;
+	p.calculate();
+	e = p.status();
+	EXPECT_EQ(8, e);
 }
